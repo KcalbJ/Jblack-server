6c152ccff4bf206ede9e69ca2bceee5fbf82c58b
kcalbj <jblack.create@gmail.com>
2023-11-20 17:14:10 +0000

    finished 5

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const { getAllTopics } = require('./controllers/topics-controller');
 
 const { handle404, handleCustomErrors, handleServerErrors, handlePsqlErrors } = require('./errors/error-handlers');
 const { getEndpoints } = require('./controllers/endpoint-controller');
-const { getArticleById } = require('./controllers/ariticles-controller');
+const { getArticleById, getAllArticles } = require('./controllers/ariticles-controller');
 
 
 
@@ -13,6 +13,7 @@ app.use(express.json())
 app.get('/api/topics', getAllTopics)
 app.get('/api', getEndpoints)
 app.get('/api/articles/:article_id', getArticleById)
+app.get('/api/articles', getAllArticles)
 
 app.all("*", handle404);
 app.use(handlePsqlErrors);
