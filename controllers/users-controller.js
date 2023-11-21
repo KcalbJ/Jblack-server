93e571c9c139e9d5487e024c69443d296102a99d
kcalbj <jblack.create@gmail.com>
2023-11-21 16:56:10 +0000

    finished 10

---
+++ controllers/users-controller.js
@@ -0,0 +1,11 @@
+const { selectUsers } = require("../models/users-models");
+
+exports.getAllUsers = (req, res, next) => {
+    selectUsers()
+      .then((users) => {
+        res.status(200).send({ users });
+      })
+      .catch(next);
+  };
+  
+  
